40cc84f0651a56d8e3eb344846e1c3ae5a30d4a8
omiy-tonser15 <omiytonser@gmail.com>
2026-02-28 14:47:17 +0300

    frontend update

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/", // Backend URL
+  baseURL: "https://project-s1uc.onrender.com/api/", // Backend URL
   headers: {
     "Content-Type": "application/json",
   },
